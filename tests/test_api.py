d51c224553dffb6230996706094b16ae3495f771
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-07-09 19:00:54 +0400

    add: basic availabilty test

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,13 @@ def test_root_route(base_url, session):
     response = session.get(f"{base_url}/")
     assert response.status_code == 200
 
+#Basic Availability Test
+def test_get_all_tasks(base_url, session):
+    response = session.get(f"{base_url}/tasks ")
+    assert response.status_code == 200
+
+
+
 
 @pytest.mark.parametrize("endpoint, expected", [
     ("/tasks", 200),
