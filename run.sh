1112b1bdbdac9298fd84db9c49e4c20aaa2904d0
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-07-17 16:44:04 +0400

    refactor: run.sh check OS, run env

--- run.sh
+++ run.sh
@@ -3,7 +3,21 @@
 #Exit Immediately if a command fails
 set -e
 
+echo "Checking for virtual environment..."
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python -m venv venv
+fi
+
+echo "Activating virtual envrionment..."
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    source venv/Scripts/activate
+else
+    source venv/bin/activate
+fi
+
 echo "Installing dependencies..."
+pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Running tests..."
