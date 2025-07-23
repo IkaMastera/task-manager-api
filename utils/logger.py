553fccc9eef71e28c09120d4796833fe5c4abc13
Irakli <112602982+IkaMastera@users.noreply.github.com>
2025-07-23 17:24:03 +0400

    refactor: logger.py for better formating

--- utils/logger.py
+++ utils/logger.py
@@ -7,9 +7,14 @@ def get_logger(name="pytest"):
     if not logger.handlers:
         formatter = logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s")
 
-        
-        file_handler = logging.FileHandler("reports/test.log",)
+        # Log to file
+        file_handler = logging.FileHandler("reports/test.log", mode="w")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
+
+        # Log to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
 
     return logger
